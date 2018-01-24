a29603a6dce58af3f449e7a9635585dc5e629f5a
Kevin Broh-Kahn <kbkrunner@Kevins-MacBook-Pro-15.local>
2018-01-24 18:44:22 -0500

    Fuck staging

--- application/views/templates/print_header.php
+++ application/views/templates/print_header.php
@@ -7,15 +7,15 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta http-equiv="Content-Type" content="text/html;charset=utf-8">
 	<meta http-equiv="Content-Language" content="en">
-	<link rel="stylesheet" href="http://staging.sixsided.com//assets/css/bootstrap.min.css">
-	<link rel="stylesheet" href="http://staging.sixsided.com//assets/css/font-awesome.min.css">
-	<link rel="stylesheet" href="http://staging.sixsided.com//assets/css/main.css">
+	<link rel="stylesheet" href="http://sixsided.com//assets/css/bootstrap.min.css">
+	<link rel="stylesheet" href="http://sixsided.com//assets/css/font-awesome.min.css">
+	<link rel="stylesheet" href="http://sixsided.com//assets/css/main.css">
 	<!-- <script data-main="/assets/js/main.js" src="/assets/js/require.js" type="text/javascript"></script> -->
-	<script type="text/javascript" src="http://staging.sixsided.com/assets/js/jquery-2.1.4.min.js"></script>
-	<script type="text/javascript" src="http://staging.sixsided.com//assets/js/bootstrap.min.js"></script>
-	<script type="text/javascript" src="http://staging.sixsided.com//assets/js/sorttable.js"></script>
-	<script type="text/javascript" src="http://staging.sixsided.com//assets/js/braintree-2.29.0.min.js"></script>
-	<script type="text/javascript" src="http://staging.sixsided.com//assets/js/helpers.js"></script>
+	<script type="text/javascript" src="http://sixsided.com/assets/js/jquery-2.1.4.min.js"></script>
+	<script type="text/javascript" src="http://sixsided.com//assets/js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="http://sixsided.com//assets/js/sorttable.js"></script>
+	<script type="text/javascript" src="http://sixsided.com//assets/js/braintree-2.29.0.min.js"></script>
+	<script type="text/javascript" src="http://sixsided.com//assets/js/helpers.js"></script>
 </head>
 <body>
 	<div class="container-fluid" id="container">
